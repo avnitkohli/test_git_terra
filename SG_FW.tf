739a3f763ec510398b950d61de3b5e4628bc08d3
avnit kohli <kohliavnit@gmail.com>
2021-04-21 16:30:50 -0500

    tet

--- SG_FW.tf
+++ SG_FW.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-
-  }
-}
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIA2ZFZX3LKIH5ZHG62"
-  secret_key = "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"
-}
-
 resource "aws_eip" "test_ip_create" {
   vpc = true
 }
